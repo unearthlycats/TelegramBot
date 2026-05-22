bbb7e26fedb1374b54efac64e82e35114dc08f6f
UnearthlyCatS <gerasimovoleksij541@gmail.com>
2026-05-22 21:58:28 +0300

    Initial commit

---
+++ src/main/kotlin/net/unearthly/telegramBot/TelegramBotManager.kt
@@ -0,0 +1,14 @@
+package net.unearthly.telegramBot
+
+import org.bukkit.plugin.java.JavaPlugin
+
+class TelegramBotManager : JavaPlugin() {
+
+    override fun onEnable() {
+        // Plugin startup logic
+    }
+
+    override fun onDisable() {
+        // Plugin shutdown logic
+    }
+}
